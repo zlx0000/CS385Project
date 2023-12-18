90d01cb3b3e33b002717464a73be69fc318ad7b4
MiddleEarthBarber <sebastianlv643@outlook.com>
2023-12-18 15:13:06 +0000

    modified:   client/node_modules/.bin/acorn 	modified:   client/node_modules/.bin/ansi-html 	modified:   client/node_modules/.bin/autoprefixer 	modified:   client/node_modules/.bin/browserslist 	modified:   client/node_modules/.bin/css-blank-pseudo 	modified:   client/node_modules/.bin/css-has-pseudo 	modified:   client/node_modules/.bin/css-prefers-color-scheme 	modified:   client/node_modules/.bin/cssesc 	modified:   client/node_modules/.bin/detect 	modified:   client/node_modules/.bin/detect-port 	modified:   client/node_modules/.bin/ejs 	modified:   client/node_modules/.bin/escodegen

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Pages/Home';
+import Search from './Pages/Search';
+import Layout from './Pages/Layout';
+import Release from './Pages/Release';
+// import Search from './Pages/Search'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search/>} />
+          <Route path="release" element={<Release/>} />
+
+
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
